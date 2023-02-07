16b71a3308b8b5694f743c69115f3d810272b341
Danijel Savkovic <danijel.savkovic@yahoo.com>
2023-02-07 14:28:57 +0100

    updated js files

--- js/game.js
+++ js/game.js
@@ -1,14 +1,10 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new MovableObject();
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext("2d");
 
-
-    character.src = '../img/1.Sharkie/3.Swim/1.png';
-
-    ctx.drawImage(character, 20, 20, 50, 150);
-
+    console.log('My character is', character);
 }
