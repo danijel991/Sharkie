71d1233a21d5f4b01d070d57c084c6cb127d1582
danijel991 <danijel.savkovic@yahoo.com>
2023-02-12 17:57:56 +0100

    animated fish

--- models/fish.class.js
+++ models/fish.class.js
@@ -3,16 +3,27 @@ class Fish extends MovableObject {
     height = 120;
     width = 130;
 
+    images_enemy = [
+        'img/2.Enemy/1.Pufferfish/1.Swim/1.swim1.png',
+        'img/2.Enemy/1.Pufferfish/1.Swim/1.swim2.png',
+        'img/2.Enemy/1.Pufferfish/1.Swim/1.swim3.png',
+        'img/2.Enemy/1.Pufferfish/1.Swim/1.swim4.png',
+        'img/2.Enemy/1.Pufferfish/1.Swim/1.swim5.png'
+    ];
+
     constructor() { //super wird geschrieben, wenn Methoden vom übergeordneten obejkt aufgerufen werden sollen
         super().loadImage('img/2.Enemy/1.Pufferfish/1.Swim/1.swim1.png');
-
+        this.loadImages(this.images_enemy);
         this.x = 200 + Math.random() * 400 - 1; //immer Zahl zwischen 200 und 700
         this.animate();
     }
 
     animate() {
         setInterval(() => {
-            this.x -= .07;
-        }, 1000/60);
+            let i = this.currentImage % this.images_enemy.length;
+            let path = this.images_enemy[i];
+            this.img = this.imgCache[path];
+            this.currentImage++;
+        }, 1000);
     }
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -5,6 +5,7 @@ class MovableObject {
     height = 150;
     width = 100;
     imgCache = {};
+    currentImage = 0;
 
     loadImage(path) {
         this.img = new Image(); //ist das Gleiche wie <img=id"" src="">
