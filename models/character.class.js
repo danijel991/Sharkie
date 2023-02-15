a74f25122ecfc533fd16d2643aa6b3871ae648b1
danijel991 <danijel.savkovic@yahoo.com>
2023-02-16 00:59:37 +0100

    moving camera

--- models/character.class.js
+++ models/character.class.js
@@ -25,13 +25,16 @@ class Character extends MovableObject {
         setInterval(() => {
             if (this.world.keyboard.RIGHT) {
                 this.x += this.speed;
+                this.otherDirection = false;
             } if (this.world.keyboard.LEFT) {
                 this.x -= this.speed;
+                this.otherDirection = true;
             } if (this.world.keyboard.UP) {
                 this.y -= this.speed;
             } if (this.world.keyboard.DOWN) {
                 this.y += this.speed;
             }
+            this.world.camera_x = -this.x;
         }, 1000 / 60);
 
         setInterval(() => {
--- models/world.class.js
+++ models/world.class.js
@@ -19,6 +19,7 @@ class World {
     canvas;
     ctx;
     keyboard;
+    camera_x = 0;
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -35,7 +36,7 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-
+        this.ctx.translate(this.camera_x, 0);
 
         this.addObjectsToMap(this.backgroundObjects);
         this.addObjectsToMap(this.floors);
@@ -43,7 +44,7 @@ class World {
         this.addObjectsToMap(this.enemies);
         this.addToMap(this.character);
 
-
+        this.ctx.translate(-this.camera_x, 0);
 
         let self = this;
         requestAnimationFrame(function () { //sobald alles darüber geladen wurde, wird das hier gezeichnet (asynchron)
@@ -58,6 +59,16 @@ class World {
     }
 
     addToMap(mo) {
+        if (mo.otherDirection) {
+            this.ctx.save();
+            this.ctx.translate(mo.width, 0);
+            this.ctx.scale(-1, 1);
+            mo.x = mo.x * -1;
+        }
         this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+        if (mo.otherDirection) {
+            mo.x = mo.x * -1;
+            this.ctx.restore();
+        }
     }
 }
