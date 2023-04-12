6e520f2f5ea90e5f8ab487922b24cf078f5ad4e9
Danijel Savkovic <danijel.savkovic@yahoo.com>
2023-04-12 22:16:25 +0200

    updated status bars

--- models/coinbar.class.js
+++ models/coinbar.class.js
@@ -1,18 +1,23 @@
 class CoinBar extends statusBar {
-    x = 10;
-    y = 50;
-    IMAGES = [
-        'img/4.Marks/green/Coin/0.png',
-        'img/4.Marks/green/Coin/20.png',
-        'img/4.Marks/green/Coin/40.png',
-        'img/4.Marks/green/Coin/60.png',
-        'img/4.Marks/green/Coin/80.png',
-        'img/4.Marks/green/Coin/100.png'
-    ];
-  
-    constructor() {
-      super();
-      this.loadImages(this.IMAGES);
-      this.setPercentage(0);
-    }
+  x = 10;
+  y = 50;
+  IMAGES = [
+    'img/4.Marks/green/Coin/0.png',
+    'img/4.Marks/green/Coin/20.png',
+    'img/4.Marks/green/Coin/40.png',
+    'img/4.Marks/green/Coin/60.png',
+    'img/4.Marks/green/Coin/80.png',
+    'img/4.Marks/green/Coin/100.png'
+  ];
+
+  constructor() {
+    super();
+    this.loadImages(this.IMAGES);
+    this.setPercentage(0);
   }
+
+  // You can add a method to update the percentage value
+  setPercentageValue(value) {
+    this.setPercentage(value);
+  }
+}
--- models/world.class.js
+++ models/world.class.js
@@ -5,12 +5,11 @@ class World {
     ctx;
     keyboard;
     camera_x = 0;
-    statusBar = [
-        new HealthBar(),
-        new CoinBar(),
-        new Poisonbar(),
-      ];
-
+    healthBar = new HealthBar();
+    coinBar = new CoinBar();
+    Poisonbar = new Poisonbar();
+    statusBar = [this.healthBar, this.coinBar, this.Poisonbar];
+  
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
@@ -29,31 +28,33 @@ class World {
             this.level.enemies.forEach((enemy) => {
                 if (this.character.isColliding(enemy)) {
                     this.character.hit();
-                    this.statusBar.setPercentage(this.character.energy);
-                    // console.log('collision with character, energy ', this.character.energy)
+                    this.healthBar.setPercentage(this.character.energy);
+                    this.coinBar.setPercentage(this.character.coins);
+                    this.Poisonbar.setPercentage(this.character.poison);
                 };
             });
         }, 1000);
     }
-
+    
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
+    
         this.ctx.translate(this.camera_x, 0);
         this.addObjectsToMap(this.level.backgroundObjects);
-
-        this.ctx.translate(-this.camera_x, 0);
-        //space for fixed objects ---
-        this.addToMap(this.statusBar);
-        this.ctx.translate(this.camera_x, 0);
-        
+    
+        // this.ctx.translate(-this.camera_x, 0);
         this.addObjectsToMap(this.level.lights);
         this.addObjectsToMap(this.level.enemies);
         this.addToMap(this.character);
-
+    
         this.ctx.translate(-this.camera_x, 0);
-
+    
+        // Draw status bars
+        this.statusBar.forEach(bar => {
+            bar.draw(this.ctx);
+        });
+    
         let self = this;
         requestAnimationFrame(function () { //sobald alles darüber geladen wurde, wird das hier gezeichnet (asynchron)
             self.draw(); //this wird hier nicht erkannt, deswegen wird staattessen self als variable vergeben
