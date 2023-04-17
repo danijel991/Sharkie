16e3ed1f1d87f870e088c9ad60cca43f7cf750db
Danijel Savkovic <danijel.savkovic@yahoo.com>
2023-04-17 21:20:26 +0200

    coin buged

--- models/jellyfish.class.js
+++ models/jellyfish.class.js
@@ -5,13 +5,6 @@ class JellyFish extends MovableObject {
 
     //Jellyfish
 
-    IMAGES_JELLYFISH_YELLOW = [
-        'img/2.Enemy/2_Jellyfish/Regular_damage/Yellow_1.png',
-        'img/2.Enemy/2_Jellyfish/Regular_damage/Yellow_2.png',
-        'img/2.Enemy/2_Jellyfish/Regular_damage/Yellow_3.png',
-        'img/2.Enemy/2_Jellyfish/Regular_damage/Yellow_4.png'
-    ];
-
     IMAGES_JELLYFISH_LILA = [
         'img/2.Enemy/2_Jellyfish/Regular_damage/Lila_1.png',
         'img/2.Enemy/2_Jellyfish/Regular_damage/Lila_2.png',
@@ -19,46 +12,11 @@ class JellyFish extends MovableObject {
         'img/2.Enemy/2_Jellyfish/Regular_damage/Lila_4.png'
     ];
 
-    IMAGES_JELLYFISH_GREEN = [
-        'img/2.Enemy/2_Jellyfish/dangerous/Green1.png',
-        'img/2.Enemy/2_Jellyfish/dangerous/Green2.png',
-        'img/2.Enemy/2_Jellyfish/dangerous/Green3.png',
-        'img/2.Enemy/2_Jellyfish/dangerous/Green4.png'
-    ];
-
-    IMAGES_JELLYFISH_PINK = [
-        'img/2.Enemy/2_Jellyfish/dangerous/Pink1.png',
-        'img/2.Enemy/2_Jellyfish/dangerous/Pink2.png',
-        'img/2.Enemy/2_Jellyfish/dangerous/Pink3.png',
-        'img/2.Enemy/2_Jellyfish/dangerous/Pink4.png'
-    ];
-
-    IMAGES_JELLYFISH_YELLOW_DEAD = [
-        'img/2.Enemy/2_Jellyfish/Dead/Yellow/y1.png',
-        'img/2.Enemy/2_Jellyfish/Dead/Yellow/y2.png',
-        'img/2.Enemy/2_Jellyfish/Dead/Yellow/y3.png',
-        'img/2.Enemy/2_Jellyfish/Dead/Yellow/y4.png'
-    ];
-
     IMAGES_JELLYFISH_LILA_DEAD = [
         'img/2.Enemy/2_Jellyfish/Dead/Lila/L1.png',
         'img/2.Enemy/2_Jellyfish/Dead/Lila/L2.png',
         'img/2.Enemy/2_Jellyfish/Dead/Lila/L3.png',
         'img/2.Enemy/2_Jellyfish/Dead/Lila/L4.png'
-    ];
-
-    IMAGES_JELLYFISH_GREEN_DEAD = [
-        'img/2.Enemy/2_Jellyfish/Dead/green/g1.png',
-        'img/2.Enemy/2_Jellyfish/Dead/green/g2.png',
-        'img/2.Enemy/2_Jellyfish/Dead/green/g3.png',
-        'img/2.Enemy/2_Jellyfish/Dead/green/g4.png'
-    ];
-
-    IMAGES_JELLYFISH_PINK_DEAD = [
-        'img/2.Enemy/2_Jellyfish/Dead/Pink/P1.png',
-        'img/2.Enemy/2_Jellyfish/Dead/Pink/P2.png',
-        'img/2.Enemy/2_Jellyfish/Dead/Pink/P3.png',
-        'img/2.Enemy/2_Jellyfish/Dead/Pink/P4.png'
     ];
 
     constructor() { //super wird geschrieben, wenn Methoden vom übergeordneten obejkt aufgerufen werden sollen
