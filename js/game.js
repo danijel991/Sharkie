1e6ad64331fcb96baa33a72df7c7c455b2d8f892
Danijel Savkovic <danijel.savkovic@yahoo.com>
2023-05-05 01:24:46 +0200

    loadingscreen

--- js/game.js
+++ js/game.js
@@ -56,7 +56,7 @@ function addStyles() {
 function loadingScreen() {
     setTimeout(() => {
         document.getElementById('loadingscreen').style.display = 'none';
-    }, 10);
+    }, 5000);
 }
 
 document.addEventListener('keydown', (e) => {
